e59cd0e76d128a3095350553f7c472afa9326b1b
Tamutnih <renu07@list.ru>
2023-04-02 19:15:14 +0500

    solution

---
+++ Heapsort.java
@@ -0,0 +1,25 @@
+package com.homework;
+
+public class Heapsort {
+    public void sort(int arr[]) {
+        int n = arr.length;
+
+        // Построение кучи (перегруппируем массив)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // Один за другим извлекаем элементы из кучи
+        for (int i = n - 1; i >= 0; i--) {
+            // Перемещаем текущий корень в конец
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Вызываем процедуру heapify на уменьшенной куче
+            heapify(arr, i, 0);
+        }
+    }
+
+    private void heapify(int[] arr, int n, int i) {
+    }
+}
